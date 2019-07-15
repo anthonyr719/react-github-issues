a1c8d1c30f52b0a026cedef9c09d20f28eacc435
Anthony Riojas <anthonyr@Anthonys-MacBook-Pro-2.local>
2019-07-14 18:04:47 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import axios from 'axios';
+import {
+  BrowserRouter as Router, 
+  Route,
+  Link
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+
+  componentDidMount() {
+    axios.get('https://api.github.com/repos/facebook/react/issues?page=1&per_page=100').then( result => {
+      console.log(result)
+      let data = result.data;
+      this.setState({
+        data
+      })
+    })
+  }
+
+
+  render() {
+    return (
+      <Router>
+        <nav>
+          <Link to="/">HOME</Link>{' '}
+        </nav>
+        <Route exact path='/' component={Home} />
+      </Router>
+    )
+  }
+};
 
 export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = props => (
+    <>
+    <div className="home">
+    <h3><span>👋 </span>Want to submit an issue to facebook/react?</h3>
+    <p>If you have a bug or an idea, read the contributing guidlines before opening an issues.</p>
+    <p>Issues labeled good first issue can be good first contributions.</p>
+    </div>
+    <button className="btn">Go to Issues</button>
+    </>
+)
+
+export default Home;
